a552c2c33b21aae397adefeb9c8b2a1e19c17e66
Johnny Zhou <jonathanqyz@gmail.com>
2022-12-16 23:38:31 -0500

    updated project section on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import Loader from "react-loaders";
 import { Link } from "react-router-dom";
 import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
+import Rehearsely from '../../assets/images/Rehearsely.png'
 
 const Home = () => { 
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -33,6 +34,14 @@ const Home = () => {
         </h1>
         <h2>Web Developer / Computer Science Student / Dancer</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+      </div>
+      <div className="project">
+        <h3>My Projects</h3>
+        <a href="https://final-project-chiyanz.vercel.app/" target='_blank'>
+          <h4>Rehearsely</h4>
+          <p>rehearsal event management app</p>
+          <img className="object-contain h-96 rounded-lg" src={Rehearsely} alt={'calendar project screenshot'} ></img>
+        </a>
       </div>
     </div>
     <Loader type="pacman" />
